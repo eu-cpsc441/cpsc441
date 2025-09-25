e9ece29d11a59e1cdd03b782b34f12a16403685d
victorbush <victorbush@gmail.com>
2025-09-25 17:18:28 -0500

    Add udpated chapter 7 scripts

---
+++ scripts/azure-sql-db/chapter7/chp7-saleco.sql
@@ -0,0 +1,237 @@
+/* Database Systems, Coronel/Morris */
+/* Type of SQL : Azure SQL Database */
+
+USE cpsc441;
+GO
+
+CREATE SCHEMA c7_saleco;
+GO
+
+CREATE TABLE c7_saleco.VENDOR
+(
+    V_CODE     INTEGER,
+    V_NAME     VARCHAR(35) NOT NULL,
+    V_CONTACT  VARCHAR(15) NOT NULL,
+    V_AREACODE CHAR(3)     NOT NULL,
+    V_PHONE    CHAR(8)     NOT NULL,
+    V_STATE    CHAR(2)     NOT NULL,
+    V_ORDER    CHAR(1)     NOT NULL,
+    PRIMARY KEY (V_CODE)
+);
+
+INSERT INTO c7_saleco.VENDOR VALUES (21225, 'Bryson, Inc.', 'Smithson', '615', '223-3234', 'TN', 'Y');
+INSERT INTO c7_saleco.VENDOR VALUES (21226, 'SuperLoo, Inc.', 'Flushing', '904', '215-8995', 'FL', 'N');
+INSERT INTO c7_saleco.VENDOR VALUES (21231, 'D&E Supply', 'Singh', '615', '228-3245', 'TN', 'Y');
+INSERT INTO c7_saleco.VENDOR VALUES (21344, 'Gomez Bros.', 'Ortega', '615', '889-2546', 'KY', 'N');
+INSERT INTO c7_saleco.VENDOR VALUES (22567, 'Dome Supply', 'Smith', '901', '678-1419', 'GA', 'N');
+INSERT INTO c7_saleco.VENDOR VALUES (23119, 'Randsets Ltd.', 'Anderson', '901', '678-3998', 'GA', 'Y');
+INSERT INTO c7_saleco.VENDOR VALUES (24004, 'Brackman Bros.', 'Browning', '615', '228-1410', 'TN', 'N');
+INSERT INTO c7_saleco.VENDOR VALUES (24288, 'ORDVA, Inc.', 'Hakford', '615', '898-1234', 'TN', 'Y');
+INSERT INTO c7_saleco.VENDOR VALUES (25443, 'B&K, Inc.', 'Smith', '904', '227-0093', 'FL', 'N');
+INSERT INTO c7_saleco.VENDOR VALUES (25501, 'Damal Supplies', 'Smythe', '615', '890-3529', 'TN', 'N');
+INSERT INTO c7_saleco.VENDOR VALUES (25595, 'Rubicon Systems', 'Orton', '904', '456-0092', 'FL', 'Y');
+
+CREATE TABLE c7_saleco.PRODUCT
+(
+    P_CODE     VARCHAR(10)
+        CONSTRAINT PRODUCT_P_CODE_PK PRIMARY KEY,
+    P_DESCRIPT VARCHAR(35)   NOT NULL,
+    P_INDATE   DATETIME      NOT NULL,
+    P_QOH      INTEGER       NOT NULL,
+    P_MIN      INTEGER       NOT NULL,
+    P_PRICE    NUMERIC(8, 2) NOT NULL,
+    P_DISCOUNT NUMERIC(4, 2) NOT NULL,
+    V_CODE     INTEGER,
+    CONSTRAINT PRODUCT_V_CODE_FK
+        FOREIGN KEY (V_CODE) REFERENCES c7_saleco.VENDOR
+);
+
+INSERT INTO c7_saleco.PRODUCT
+VALUES ('11QER/31', 'Power painter, 15 psi., 3-nozzle', '03-NOV-2021', 8, 5, 109.99, 0.00, 25595);
+INSERT INTO c7_saleco.PRODUCT VALUES ('13-Q2/P2', '7.25-in. pwr. saw blade', '13-DEC-2021', 32, 15, 14.99, 0.05, 21344);
+INSERT INTO c7_saleco.PRODUCT VALUES ('14-Q1/L3', '9.00-in. pwr. saw blade', '13-NOV-2021', 18, 12, 17.49, 0.00, 21344);
+INSERT INTO c7_saleco.PRODUCT VALUES ('1546-QQ2', 'Hrd. cloth, 1/4-in., 2x50', '15-JAN-2022', 15, 8, 39.95, 0.00, 23119);
+INSERT INTO c7_saleco.PRODUCT VALUES ('1558-QW1', 'Hrd. cloth, 1/2-in., 3x50', '15-JAN-2022', 23, 5, 43.99, 0.00, 23119);
+INSERT INTO c7_saleco.PRODUCT VALUES ('2232/QTY', 'B&D jigsaw, 12-in. blade', '30-DEC-2021', 8, 5, 109.92, 0.05, 24288);
+INSERT INTO c7_saleco.PRODUCT VALUES ('2232/QWE', 'B&D jigsaw, 8-in. blade', '24-DEC-2021', 6, 5, 99.87, 0.05, 24288);
+INSERT INTO c7_saleco.PRODUCT
+VALUES ('2238/QPD', 'B&D cordless drill, 1/2-in.', '20-JAN-2022', 12, 5, 38.95, 0.05, 25595);
+INSERT INTO c7_saleco.PRODUCT VALUES ('23109-HB', 'Claw hammer', '20-JAN-2022', 23, 10, 9.95, 0.10, 21225);
+INSERT INTO c7_saleco.PRODUCT VALUES ('23114-AA', 'Sledge hammer, 12 lb.', '02-JAN-2022', 8, 5, 14.40, 0.05, NULL);
+INSERT INTO c7_saleco.PRODUCT
+VALUES ('54778-2T', 'Rat-tail file, 1/8-in. fine', '15-DEC-2021', 43, 20, 4.99, 0.00, 21344);
+INSERT INTO c7_saleco.PRODUCT VALUES ('89-WRE-Q', 'Hicut chain saw, 16 in.', '07-FEB-2022', 11, 5, 256.99, 0.05, 24288);
+INSERT INTO c7_saleco.PRODUCT VALUES ('PVC23DRT', 'PVC pipe, 3.5-in., 8-ft', '20-FEB-2022', 188, 75, 5.87, 0.00, NULL);
+INSERT INTO c7_saleco.PRODUCT VALUES ('SM-18277', '1.25-in. metal screw, 25', '01-MAR-2022', 172, 75, 6.99, 0.00, 21225);
+INSERT INTO c7_saleco.PRODUCT VALUES ('SW-23116', '2.5-in. wd. screw, 50', '24-FEB-2022', 237, 100, 8.45, 0.00, 21231);
+INSERT INTO c7_saleco.PRODUCT
+VALUES ('WR3/TT3', 'Steel matting, 4''x8''x1/6", .5" mesh', '17-JAN-2022', 18, 5, 119.95, 0.10, 25595);
+
+CREATE TABLE c7_saleco.CUSTOMER
+(
+    CUS_CODE     INTEGER PRIMARY KEY,
+    CUS_LNAME    VARCHAR(15) NOT NULL,
+    CUS_FNAME    VARCHAR(15) NOT NULL,
+    CUS_INITIAL  CHAR(1),
+    CUS_AREACODE CHAR(3),
+    CUS_PHONE    CHAR(8)     NOT NULL,
+    CUS_BALANCE  NUMERIC(9, 2) DEFAULT 0.00,
+    CONSTRAINT CUS_UI1 UNIQUE (CUS_LNAME, CUS_FNAME, CUS_PHONE)
+);
+
+INSERT INTO c7_saleco.CUSTOMER VALUES (10010, 'Ramas', 'Alfred', 'A', '615', '844-2573', 0);
+INSERT INTO c7_saleco.CUSTOMER VALUES (10011, 'Dunne', 'Leona', 'K', '713', '894-1238', 0);
+INSERT INTO c7_saleco.CUSTOMER VALUES (10012, 'Smith', 'Kathy', 'W', '615', '894-2285', 345.86);
+INSERT INTO c7_saleco.CUSTOMER VALUES (10013, 'Olowski', 'Paul', 'F', '615', '894-2180', 536.75);
+INSERT INTO c7_saleco.CUSTOMER VALUES (10014, 'Orlando', 'Myron', NULL, '615', '222-1672', 0);
+INSERT INTO c7_saleco.CUSTOMER VALUES (10015, 'O''Brian', 'Amy', 'B', '713', '442-3381', 0);
+INSERT INTO c7_saleco.CUSTOMER VALUES (10016, 'Brown', 'James', 'G', '615', '297-1228', 221.19);
+INSERT INTO c7_saleco.CUSTOMER VALUES (10017, 'Williams', 'George', NULL, '615', '290-2556', 768.93);
+INSERT INTO c7_saleco.CUSTOMER VALUES (10018, 'Farriss', 'Anne', 'G', '713', '382-7185', 216.55);
+INSERT INTO c7_saleco.CUSTOMER VALUES (10019, 'Smith', 'Olette', 'K', '615', '297-3809', 0);
+
+CREATE TABLE c7_saleco.INVOICE
+(
+    INV_NUMBER INTEGER PRIMARY KEY,
+    CUS_CODE   INTEGER                            NOT NULL REFERENCES c7_saleco.CUSTOMER (CUS_CODE),
+    INV_DATE   DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT INV_CK1 CHECK (INV_DATE > '2010-01-01')
+);
+
+INSERT INTO c7_saleco.INVOICE VALUES (1001, 10014, '16-JAN-2022');
+INSERT INTO c7_saleco.INVOICE VALUES (1002, 10011, '16-JAN-2022');
+INSERT INTO c7_saleco.INVOICE VALUES (1003, 10012, '16-JAN-2022');
+INSERT INTO c7_saleco.INVOICE VALUES (1004, 10011, '17-JAN-2022');
+INSERT INTO c7_saleco.INVOICE VALUES (1005, 10018, '17-JAN-2022');
+INSERT INTO c7_saleco.INVOICE VALUES (1006, 10014, '17-JAN-2022');
+INSERT INTO c7_saleco.INVOICE VALUES (1007, 10015, '17-JAN-2022');
+INSERT INTO c7_saleco.INVOICE VALUES (1008, 10011, '17-JAN-2022');
+
+
+CREATE TABLE c7_saleco.LINE
+(
+    INV_NUMBER  INTEGER                    NOT NULL,
+    LINE_NUMBER NUMERIC(2, 0)              NOT NULL,
+    P_CODE      VARCHAR(10)                NOT NULL,
+    LINE_UNITS  NUMERIC(9, 2) DEFAULT 0.00 NOT NULL,
+    LINE_PRICE  NUMERIC(9, 2) DEFAULT 0.00 NOT NULL,
+    PRIMARY KEY (INV_NUMBER, LINE_NUMBER),
+    FOREIGN KEY (INV_NUMBER) REFERENCES c7_saleco.INVOICE ON DELETE CASCADE,
+    FOREIGN KEY (P_CODE) REFERENCES c7_saleco.PRODUCT (P_CODE),
+    CONSTRAINT LINE_UI1 UNIQUE (INV_NUMBER, P_CODE)
+);
+
+INSERT INTO c7_saleco.LINE VALUES (1001, 1, '13-Q2/P2', 1, 14.99);
+INSERT INTO c7_saleco.LINE VALUES (1001, 2, '23109-HB', 1, 9.95);
+INSERT INTO c7_saleco.LINE VALUES (1002, 1, '54778-2T', 2, 4.99);
+INSERT INTO c7_saleco.LINE VALUES (1003, 1, '2238/QPD', 1, 38.95);
+INSERT INTO c7_saleco.LINE VALUES (1003, 2, '1546-QQ2', 1, 39.95);
+INSERT INTO c7_saleco.LINE VALUES (1003, 3, '13-Q2/P2', 5, 14.99);
+INSERT INTO c7_saleco.LINE VALUES (1004, 1, '54778-2T', 3, 4.99);
+INSERT INTO c7_saleco.LINE VALUES (1004, 2, '23109-HB', 2, 9.95);
+INSERT INTO c7_saleco.LINE VALUES (1005, 1, 'PVC23DRT', 12, 5.87);
+INSERT INTO c7_saleco.LINE VALUES (1006, 1, 'SM-18277', 3, 6.99);
+INSERT INTO c7_saleco.LINE VALUES (1006, 2, '2232/QTY', 1, 109.92);
+INSERT INTO c7_saleco.LINE VALUES (1006, 3, '23109-HB', 1, 9.95);
+INSERT INTO c7_saleco.LINE VALUES (1006, 4, '89-WRE-Q', 1, 256.99);
+INSERT INTO c7_saleco.LINE VALUES (1007, 1, '13-Q2/P2', 2, 14.99);
+INSERT INTO c7_saleco.LINE VALUES (1007, 2, '54778-2T', 1, 4.99);
+INSERT INTO c7_saleco.LINE VALUES (1008, 1, 'PVC23DRT', 5, 5.87);
+INSERT INTO c7_saleco.LINE VALUES (1008, 2, 'WR3/TT3', 3, 119.95);
+INSERT INTO c7_saleco.LINE VALUES (1008, 3, '23109-HB', 1, 9.95);
+
+CREATE TABLE c7_saleco.EMPLOYEE
+(
+    EMP_NUM       INTEGER PRIMARY KEY,
+    EMP_TITLE     CHAR(10),
+    EMP_LNAME     VARCHAR(15) NOT NULL,
+    EMP_FNAME     VARCHAR(15) NOT NULL,
+    EMP_INITIAL   CHAR(1),
+    EMP_DOB       DATETIME,
+    EMP_HIRE_DATE DATETIME,
+    EMP_YEARS     INTEGER,
+    EMP_AREACODE  CHAR(3),
+    EMP_PHONE     CHAR(8)
+);
+
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (100, 'Mr.', 'Kolmycz', 'George', 'D', '15-JUN-1967', '15-MAR-2010', 11, '615', '324-5456');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (101, 'Ms.', 'Lewis', 'Rhonda', 'G', '19-MAR-1990', '25-APR-2011', 10, '615', '324-4472');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (102, 'Mr.', 'Vandam', 'Rhett', NULL, '14-NOV-1983', '20-DEC-2015', 6, '901', '675-8993');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (103, 'Ms.', 'Jones', 'Anne', 'M', '16-OCT-1999', '28-AUG-2019', 2, '615', '898-3456');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (104, 'Mr.', 'Lange', 'John', 'P', '08-NOV-1996', '20-OCT-2019', 2, '901', '504-4430');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (105, 'Mr.', 'Williams', 'Robert', 'D', '14-MAR-2000', '08-NOV-2020', 1, '615', '890-3220');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (106, 'Mrs.', 'Smith', 'Jeanine', 'K', '12-FEB-1993', '05-JAN-2014', 7, '615', '324-7883');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (107, 'Mr.', 'Diante', 'Jorge', 'D', '21-AUG-1999', '02-JUL-2019', 2, '615', '890-4567');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (108, 'Mr.', 'Wiesenbach', 'Paul', 'R', '14-FEB-1991', '18-NOV-2017', 4, '615', '897-4358');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (109, 'Mr.', 'Smith', 'George', 'K', '18-JUN-1986', '14-APR-2014', 7, '901', '504-3339');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (110, 'Mrs.', 'Genkazi', 'Leighla', 'W', '19-MAY-1995', '01-DEC-2015', 6, '901', '569-0093');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (111, 'Mr.', 'Washington', 'Rupert', 'E', '03-JAN-1991', '21-JUN-2018', 3, '615', '890-4925');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (112, 'Mr.', 'Johnson', 'Edward', 'E', '14-MAY-1986', '01-DEC-2008', 13, '615', '898-4387');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (113, 'Ms.', 'Smythe', 'Melanie', 'P', '15-SEP-1995', '11-MAY-2020', 1, '615', '324-9006');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (114, 'Ms.', 'Brandon', 'Marie', 'G', '02-NOV-1981', '15-NOV-2004', 17, '901', '882-0845');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (115, 'Mrs.', 'Saranda', 'Hermine', 'R', '25-JUL-1997', '23-APR-2018', 3, '615', '324-5505');
+INSERT INTO c7_saleco.EMPLOYEE
+VALUES (116, 'Mr.', 'Smith', 'George', 'A', '08-NOV-1990', '10-DEC-2013', 8, '615', '890-2984');
+
+CREATE TABLE c7_saleco.EMP
+(
+    EMP_NUM       INTEGER PRIMARY KEY,
+    EMP_TITLE     CHAR(10),
+    EMP_LNAME     VARCHAR(15) NOT NULL,
+    EMP_FNAME     VARCHAR(15) NOT NULL,
+    EMP_INITIAL   CHAR(1),
+    EMP_DOB       DATETIME,
+    EMP_HIRE_DATE DATETIME,
+    EMP_AREACODE  CHAR(3),
+    EMP_PHONE     CHAR(8),
+    EMP_MGR       INTEGER
+);
+
+INSERT INTO c7_saleco.EMP
+VALUES (100, 'Mr.', 'Kolmycz', 'George', 'D', '15-JUN-1967', '15-MAR-2010', '615', '324-5456', NULL);
+INSERT INTO c7_saleco.EMP
+VALUES (101, 'Ms.', 'Lewis', 'Rhonda', 'G', '19-MAR-1990', '25-APR-2011', '615', '324-4472', 100);
+INSERT INTO c7_saleco.EMP
+VALUES (102, 'Mr.', 'Vandam', 'Rhett', NULL, '14-NOV-1983', '20-DEC-2015', '901', '675-8993', 100);
+INSERT INTO c7_saleco.EMP VALUES (103, 'Ms.', 'Jones', 'Anne', 'M', '16-OCT-1999', '28-AUG-2019', '615', '898-3456', 100);
+INSERT INTO c7_saleco.EMP VALUES (104, 'Mr.', 'Lange', 'John', 'P', '08-NOV-1996', '20-OCT-2019', '901', '504-4430', 105);
+INSERT INTO c7_saleco.EMP
+VALUES (105, 'Mr.', 'Williams', 'Robert', 'D', '14-MAR-2000', '08-NOV-2020', '615', '890-3220', NULL);
+INSERT INTO c7_saleco.EMP
+VALUES (106, 'Mrs.', 'Smith', 'Jeanine', 'K', '12-FEB-1993', '05-JAN-2014', '615', '324-7883', 105);
+INSERT INTO c7_saleco.EMP
+VALUES (107, 'Mr.', 'Diante', 'Jorge', 'D', '21-AUG-1999', '02-JUL-2019', '615', '890-4567', 105);
+INSERT INTO c7_saleco.EMP
+VALUES (108, 'Mr.', 'Wiesenbach', 'Paul', 'R', '14-FEB-1991', '18-NOV-2017', '615', '897-4358', NULL);
+INSERT INTO c7_saleco.EMP
+VALUES (109, 'Mr.', 'Smith', 'George', 'K', '18-JUN-1986', '14-APR-2014', '901', '504-3339', 108);
+INSERT INTO c7_saleco.EMP
+VALUES (110, 'Mrs.', 'Genkazi', 'Leighla', 'W', '19-MAY-1995', '01-DEC-2015', '901', '569-0093', 108);
+INSERT INTO c7_saleco.EMP
+VALUES (111, 'Mr.', 'Washington', 'Rupert', 'E', '03-JAN-1991', '21-JUN-2018', '615', '890-4925', 105);
+INSERT INTO c7_saleco.EMP
+VALUES (112, 'Mr.', 'Johnson', 'Edward', 'E', '14-MAY-1986', '01-DEC-2008', '615', '898-4387', 100);
+INSERT INTO c7_saleco.EMP
+VALUES (113, 'Ms.', 'Smythe', 'Melanie', 'P', '15-SEP-1995', '11-MAY-2020', '615', '324-9006', 105);
+INSERT INTO c7_saleco.EMP
+VALUES (114, 'Ms.', 'Brandon', 'Marie', 'G', '02-NOV-1981', '15-NOV-2004', '901', '882-0845', 108);
+INSERT INTO c7_saleco.EMP
+VALUES (115, 'Mrs.', 'Saranda', 'Hermine', 'R', '25-JUL-1997', '23-APR-2018', '615', '324-5505', 105);
+INSERT INTO c7_saleco.EMP
+VALUES (116, 'Mr.', 'Smith', 'George', 'A', '08-NOV-1990', '10-DEC-2013', '615', '890-2984', 108);
